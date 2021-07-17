7933e465e0be87041766b587ab833cef3175a209
YutaSugimura <nextc3m@gmail.com>
2021-07-17 16:31:17 +0900

    fix: screens/index

---
+++ src/components/account/current.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import type { Wallet } from '../../types/wallet';
+import { formatEther } from '../../utils/formatEther';
+
+type Props = {};
+
+export const CurrentAccount: React.VFC<Props> = () => {
+  require('../../hooks/wallet/switching').useSwitchingEffect();
+  const { wallet }: { wallet: Wallet } = require('../../context/wallet').useWalletState();
+  const { balance }: { balance: string } = require('../../hooks/wallet/balance').useBalance();
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>Address</Text>
+        <Text style={styles.value}>{wallet !== null ? wallet.address : ''}</Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.title}>PrivateKey</Text>
+        <Text style={styles.value}>{wallet !== null ? wallet.privateKey : ''}</Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.title}>Balance</Text>
+        <Text style={styles.value}>{balance !== '' ? formatEther(balance) : ''}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+  section: {
+    paddingTop: 10,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  value: {
+    color: 'rgba(0, 0, 0, 0.8)',
+    fontSize: 14,
+  },
+});
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  ActivityIndicator,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -9,22 +8,15 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import { CurrentAccount } from '../components/account/current';
 import { Button } from '../components/button';
 import { RootNavigationProp } from '../navigation';
-
-import type { Wallet } from '../types/wallet';
-import { formatEther } from '../utils/formatEther';
 
 type Props = {};
 
 const Screen: React.VFC<Props> = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const navigation: RootNavigationProp<'Top'> = require('@react-navigation/native').useNavigation();
-  const { isLoading } = require('../hooks/loading').useIsLoading();
-
-  require('../hooks/wallet/switching').useSwitchingEffect();
-  const { wallet }: { wallet: Wallet } = require('../context/wallet').useWalletState();
-  const { balance }: { balance: string } = require('../hooks/wallet/balance').useBalance();
 
   const createWallet = () => {
     navigation.navigate('CreateWallet');
@@ -50,11 +42,7 @@ const Screen: React.VFC<Props> = () => {
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        {isLoading && <ActivityIndicator size="large" />}
-
-        <Text>Address: {wallet !== null ? wallet.address : ''}</Text>
-        <Text>PrivateKey: {wallet !== null ? wallet.privateKey : ''}</Text>
-        <Text>Balance: {balance !== '' ? formatEther(balance) : ''}</Text>
+        <CurrentAccount />
 
         <View style={styles.container}>
           <View style={styles.section}>
