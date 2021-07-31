e5036dd40ddb07d4ac29fafdb530a7d6b3587cea
YutaSugimura <nextc3m@gmail.com>
2021-08-01 00:09:26 +0900

    fix

--- src/hooks/provider/index.ts
+++ src/hooks/provider/index.ts
@@ -1,20 +1,36 @@
 import type Ethers from 'ethers';
 import { useSetRecoilState } from 'recoil';
 import { providerState } from '../../recoil/atoms/provider';
+import { networkState } from '../../recoil/atoms/network';
 
 const ethers: typeof Ethers = require('ethers');
 
 export const useProvider = () => {
-  const setState = useSetRecoilState(providerState);
+  const setProviderState = useSetRecoilState(providerState);
+  const setNetworkState = useSetRecoilState(networkState);
 
-  const setBaseProvider = (network: string) => {
+  const setBaseProvider = async (network: string) => {
     const newProvider = ethers.getDefaultProvider(network);
-    setState(newProvider);
+    const newNetwork = await newProvider.getNetwork();
+
+    setProviderState(newProvider);
+    setNetworkState({
+      chainId: newNetwork.chainId,
+      name: newNetwork.name,
+      isJsonRpcProvider: false,
+    });
   };
 
   const setJsonRpcProvider = async (customUrl: string) => {
     const newProvider = new ethers.providers.JsonRpcProvider(customUrl);
-    setState(newProvider);
+    const newNetwork = await newProvider.getNetwork();
+
+    setProviderState(newProvider);
+    setNetworkState({
+      chainId: newNetwork.chainId,
+      name: newNetwork.name,
+      isJsonRpcProvider: true,
+    });
   };
 
   return {
