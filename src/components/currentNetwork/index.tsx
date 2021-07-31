a66bbc7fa55c6a93c0a3f03e511a87a4c888c2ad
YutaSugimura <nextc3m@gmail.com>
2021-08-01 00:15:23 +0900

    fix

--- src/components/currentNetwork/index.tsx
+++ src/components/currentNetwork/index.tsx
@@ -1,31 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import { useRecoilValue } from 'recoil';
-import { providerState } from '../../recoil/atoms/provider';
-import { getNetwork } from '../../utils/getNetwork';
+import { networkState } from '../../recoil/atoms/network';
 
 type Props = {};
 
 export const CurrentNetwork: React.VFC<Props> = () => {
-  const provider = useRecoilValue(providerState);
-  const [state, setState] = useState<string>('');
-
-  useEffect(() => {
-    (async () => {
-      if (provider === null) {
-        return;
-      }
-
-      const network = await getNetwork(provider);
-      if (network) {
-        setState(network.name);
-      }
-    })();
-  }, [provider]);
+  const networkValue = useRecoilValue(networkState);
 
   return (
     <View>
-      <Text>Current Network: {state}</Text>
+      <Text>Current Network</Text>
+      <Text>chainId: {networkValue.chainId}</Text>
+      <Text>name: {networkValue.name}</Text>
+      <Text>isJsonRpcProvider {networkValue.isJsonRpcProvider}</Text>
     </View>
   );
 };
