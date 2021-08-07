b69c3f81f696ebd857b2e416d2df4fb9718c383a
YutaSugimura <nextc3m@gmail.com>
2021-08-07 23:48:25 +0900

    clean: recoil/atoms/account

--- src/hooks/account/list.ts
+++ src/hooks/account/list.ts
@@ -1,8 +1,8 @@
 import { useRecoilState } from 'recoil';
-import { accountState } from '../../recoil/atoms/account';
+import { accountListState } from '../../recoil/atoms/account';
 
 export const useAccountlist = () => {
-  const [state, setState] = useRecoilState(accountState);
+  const [state, setState] = useRecoilState(accountListState);
 
   const addAccountList = (address: string, privateKey: string, label: string) => {
     setState([...state, { address, privateKey, label }]);
--- src/recoil/atoms/account.ts
+++ src/recoil/atoms/account.ts
@@ -1,7 +1,32 @@
 import { atom } from 'recoil';
 import type { Accounts } from '../../types/account';
 
-export const accountState = atom({
-  key: 'accountState',
+export const accountListState = atom({
+  key: 'accountListState',
   default: [] as Accounts,
 });
+
+/**
+ * accountList
+ *
+ * [
+ *   {
+ *     label: 'main',
+ *     address: 0x123...,
+ *   },
+ *     label: 'sub',
+ *     address: 0x123...,
+ *   },
+ * ]
+ *
+ * storage
+ * key: mnemonic,
+ * value: 'word rock stone ...',
+ *
+ * key: account/${LABEL}
+ * value: {
+ *  type: 'privatekey',
+ *  key: '0x1234...',
+ * }
+ *
+ */
